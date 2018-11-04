abac91104715b835b4db7efb1c876ad80659a619
jako <tranjak@yahoo.fr>
2018-11-05 00:21:24 +0100

    Adding frontend files to Github pages 2

--- deployfrontend.cmd
+++ deployfrontend.cmd
@@ -1,5 +1,5 @@
 robocopy src docs /e
 robocopy build\contracts docs
 git add .
-git commit -m "Adding frontend files to Github pages"
+git commit -m "Adding frontend files to Github pages 2"
 git push
